0ebaa9af795a86113c8aff9b6b71e0cdb5685c1f
TihonV <tihonmmm2@gmail.com>
2026-01-29 11:55:15 +0300

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   content: { type: String, required: true, maxlength: 280 },
+  comments: [{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    content: String,
+    timestamp: { type: Date, default: Date.now }
+  }],
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  createdAt: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Post', PostSchema);
