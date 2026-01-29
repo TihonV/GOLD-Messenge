1516ebff6f53185d133a6d15f58a3d4adf0257c3
TihonV <tihonmmm2@gmail.com>
2026-01-29 11:12:28 +0300

    Create Channel.js

---
+++ backend/models/Channel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const ChannelSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: String,
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Channel', ChannelSchema);
